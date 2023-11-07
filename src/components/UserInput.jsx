8f11099e324cecadca01d7ff6b5ba046ed9a772f
Parthik <parthikrudakiya001@gmail.com>
2023-11-07 18:01:36 +0530

    Include User Input component and implementation

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+export default function UserInput() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleInputChange(inputIdentifier, newValue) {
+    setUserInput((preInput) => {
+      return {
+        ...preInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
+  // console.log(userInput);
+
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+              handleInputChange("initialInvestment", event.target.value)
+            }
+            required
+          />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+              handleInputChange("annualInvestment", event.target.value)
+            }
+            required
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            value={userInput.expectedReturn}
+            onChange={(event) =>
+              handleInputChange("expectedReturn", event.target.value)
+            }
+            required
+          />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input
+            type="number"
+            value={userInput.duration}
+            onChange={(event) =>
+              handleInputChange("duration", event.target.value)
+            }
+            required
+          />
+        </p>
+      </div>
+    </section>
+  );
+}
